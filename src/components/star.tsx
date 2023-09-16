f1c276933eb6f5ba8bc2f001e3f3bc59f8b87880
Nathanael <Nathan@MacBook-Pro.local>
2023-09-16 19:15:54 +0700

    fix star

--- src/components/star.tsx
+++ src/components/star.tsx
@@ -1,13 +1,31 @@
+import { BiSolidStar, BiSolidStarHalf } from "react-icons/bi";
+
 interface RatingStarsProps {
   rating: string;
 }
 
 export default function RatingStars({ rating }: RatingStarsProps) {
-  const numStars = parseInt(rating);
-  const stars = Array.from({ length: numStars }, (_, index) => (
-    <span key={index} className="text-blue-950">
-      &#9733;
-    </span>
-  ));
-  return <div className="text-xs">{stars}</div>;
+  const numericRating = parseFloat(rating);
+  const fullStars = Math.floor(numericRating);
+  const hasHalfStar = numericRating - fullStars >= 0.5;
+
+  const stars = [];
+
+  for (let i = 0; i < fullStars; i++) {
+    stars.push(
+      <span key={i} className="text-blue-950">
+        <BiSolidStar />
+      </span>
+    );
+  }
+
+  if (hasHalfStar) {
+    stars.push(
+      <span key="half" className="text-blue-950 flex gap-[2px]">
+        <BiSolidStar /> <BiSolidStarHalf />
+      </span>
+    );
+  }
+
+  return <div className="text-xs flex gap-[2px]">{stars}</div>;
 }
