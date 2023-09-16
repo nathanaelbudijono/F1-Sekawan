d59142c90fe73e1f1985485bda39f4781ceef182
Nathanael <Nathan@MacBook-Pro.local>
2023-09-16 19:26:40 +0700

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Card from "./components/card";
 import SearchFilter from "./components/search/search-filter";
 import SortListBox from "./components/search/sort-list-box";
 import Typography from "./components/typography";
+import Footer from "./components/footer";
 
 export default function App() {
   const { restaurants, isLoading } = useRestaurant();
@@ -22,31 +23,34 @@ export default function App() {
     );
   }
   return (
-    <Layout className="flex flex-col">
-      <Header />
-      <main className="mt-5">
-        <SearchFilter
-          setFiltered={setFiltered}
-          restaurants={restaurants}
-          selected={selected}
-        />
-        <SortListBox selected={selected} setSelected={setSelected} />
-      </main>
-      <main className="mt-5">
-        <section className="grid grid-cols-4 gap-2 max-md:grid-cols-2 max-sm:grid-cols-1">
-          {filtered?.map((item) => {
-            return (
-              <Card
-                id={item?.id}
-                rating={item?.rating}
-                name={item?.name}
-                city={item?.city}
-                description={item?.description}
-              />
-            );
-          })}
-        </section>
-      </main>
-    </Layout>
+    <>
+      <Layout className="flex flex-col">
+        <Header />
+        <main className="mt-5">
+          <SearchFilter
+            setFiltered={setFiltered}
+            restaurants={restaurants}
+            selected={selected}
+          />
+          <SortListBox selected={selected} setSelected={setSelected} />
+        </main>
+        <main className="mt-5">
+          <section className="grid grid-cols-4 gap-2 max-md:grid-cols-2 max-sm:grid-cols-1">
+            {filtered?.map((item) => {
+              return (
+                <Card
+                  id={item?.id}
+                  rating={item?.rating}
+                  name={item?.name}
+                  city={item?.city}
+                  description={item?.description}
+                />
+              );
+            })}
+          </section>
+        </main>
+      </Layout>
+      <Footer />
+    </>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,28 @@
+import Typography from "./typography";
+
+import { AiFillMail, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+
+export default function Footer() {
+  return (
+    <footer className="max-w-5xl mx-auto flex py-4 max-md:py-3 px-16 max-md:px-6 h-[20vh] text-center">
+      <section className="max-w-4xl mx-auto flex justify-center flex-col items-center gap-1">
+        <div>
+          <Typography variant="small" className="text-xs">
+            © {new Date().getFullYear()} All right reserved PT Tambang Jaya
+          </Typography>
+        </div>
+        <div className="flex gap-2">
+          <a href="mailto:nathanaelbudijono@gmail.com">
+            <AiFillMail className="icon" />
+          </a>
+          <a href="https://github.com/nathanaelbudijono">
+            <AiFillGithub className="icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/nathanael-budijono/">
+            <AiFillLinkedin className="icon" />
+          </a>
+        </div>
+      </section>
+    </footer>
+  );
+}
